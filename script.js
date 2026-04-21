801fca8db5d5cd41c769bf153b4a48c78a665760
marisloading <mar.pendao@hotmail.com>
2026-04-21 13:48:40 +0100

    Add UI to the game

--- script.js
+++ script.js
@@ -1,65 +1,119 @@
-playGame();
+const buttons = document.querySelector("#selection");
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+buttons.addEventListener("click", (event) => playRound(event.target.id));
 
-  while (humanScore < 5 && computerScore < 5) {
-    let computerSelection = getComputerChoice();
-    let playerSelection = getHumanChoice();
-    let roundMessage = "";
+function playRound(playerSelection) {
+  const resultDiv = document.getElementById("results");
+  const winnerMessage = document.createElement("h2");
+  const roundWinner = document.createElement("h2");
+  const tallyCount = document.createElement("p");
+  const newRoundBtn = document.createElement("button");
 
-    if (computerSelection === "rock") {
-      if (playerSelection === "rock") {
-        roundMessage = "It's a tie!";
-      } else if (playerSelection === "paper") {
-        humanScore++;
-        roundMessage = "Paper beats Rock, you win!";
-      } else if (playerSelection === "scissors") {
-        computerScore++;
-        roundMessage = "Rock beats Scissors, you lost...";
-      } else {
-        roundMessage = "Something went wrong.";
-      }
-    } else if (computerSelection === "paper") {
-      if (playerSelection === "rock") {
-        computerScore++;
-        roundMessage = "Paper beats Rock, you lost...";
-      } else if (playerSelection === "paper") {
-        roundMessage = "It's a tie!";
-      } else if (playerSelection === "scissors") {
-        humanScore++;
-        roundMessage = "Scissors beats Paper, you win!";
-      } else {
-        roundMessage = "Something went wrong.";
-      }
-    } else if (computerSelection === "scissors") {
-      if (playerSelection === "rock") {
-        humanScore++;
-        roundMessage = "Rock beats Scissors, you win!";
-      } else if (playerSelection === "paper") {
-        computerScore++;
-        roundMessage = "Scissors beats Paper, you lost...";
-      } else if (playerSelection === "scissors") {
-        roundMessage = "It's a tie!";
-      } else {
-        roundMessage = "Something went wrong.";
-      }
+  let computerSelection = getComputerChoice();
+  let roundMessage = "";
+
+  if (computerSelection === "rock") {
+    if (playerSelection === "rock") {
+      roundMessage = "It's a tie!";
+    } else if (playerSelection === "paper") {
+      humanScore++;
+      roundMessage = "Paper beats Rock, you win!";
+    } else if (playerSelection === "scissors") {
+      computerScore++;
+      roundMessage = "Rock beats Scissors, you lost...";
     } else {
       roundMessage = "Something went wrong.";
     }
+  } else if (computerSelection === "paper") {
+    if (playerSelection === "rock") {
+      computerScore++;
+      roundMessage = "Paper beats Rock, you lost...";
+    } else if (playerSelection === "paper") {
+      roundMessage = "It's a tie!";
+    } else if (playerSelection === "scissors") {
+      humanScore++;
+      roundMessage = "Scissors beats Paper, you win!";
+    } else {
+      roundMessage = "Something went wrong.";
+    }
+  } else if (computerSelection === "scissors") {
+    if (playerSelection === "rock") {
+      humanScore++;
+      roundMessage = "Rock beats Scissors, you win!";
+    } else if (playerSelection === "paper") {
+      computerScore++;
+      roundMessage = "Scissors beats Paper, you lost...";
+    } else if (playerSelection === "scissors") {
+      roundMessage = "It's a tie!";
+    } else {
+      roundMessage = "Something went wrong.";
+    }
+  } else {
+    roundMessage = "Something went wrong.";
+  }
 
-    console.log(roundMessage);
-    console.log("Human score: " + humanScore);
-    console.log("Computer score: " + computerScore);
+  if (resultDiv.hasChildNodes()) {
+    const lastTurnMsg = document.querySelector("#message");
+    const lastTurnTally = document.querySelector("#tally");
+
+    winnerMessage.textContent = roundMessage;
+    tallyCount.textContent = `Human: ${humanScore} / Computer: ${computerScore}`;
+
+    resultDiv.replaceChild(winnerMessage, lastTurnMsg);
+    resultDiv.replaceChild(tallyCount, lastTurnTally);
+
+    winnerMessage.setAttribute("id", "message");
+    tallyCount.setAttribute("id", "tally");
+  } else {
+    resultDiv.appendChild(winnerMessage);
+    resultDiv.appendChild(tallyCount);
+
+    winnerMessage.textContent = roundMessage;
+    winnerMessage.setAttribute("id", "message");
+
+    tallyCount.textContent = `Human: ${humanScore} / Computer: ${computerScore}`;
+    tallyCount.setAttribute("id", "tally");
   }
 
   if (humanScore === 5) {
-    console.log("You won! You beat the computer!");
+    resultDiv.appendChild(roundWinner);
+    resultDiv.removeChild(tallyCount);
+    resultDiv.removeChild(winnerMessage);
+
+    roundWinner.textContent = "You won all 5 rounds!";
+
+    resultDiv.appendChild(newRoundBtn);
+    newRoundBtn.textContent = "New round";
+
+    buttons.style.display = "none";
+
+    newRoundBtn.addEventListener("click", () => {
+      resultDiv.removeChild(newRoundBtn);
+      resultDiv.removeChild(roundWinner);
+      humanScore = 0;
+      computerScore = 0;
+      buttons.style.display = "block";
+    });
   } else if (computerScore === 5) {
-    console.log("You lost! The computer is better than you.");
-  } else {
-    console.log("Something went wrong.");
+    resultDiv.appendChild(roundWinner);
+    resultDiv.removeChild(tallyCount);
+    resultDiv.removeChild(winnerMessage);
+
+    roundWinner.textContent = "You lost. The computer is better than you.";
+
+    resultDiv.appendChild(newRoundBtn);
+    newRoundBtn.textContent = "New round";
+    buttons.style.display = "none";
+
+    newRoundBtn.addEventListener("click", () => {
+      resultDiv.removeChild(newRoundBtn);
+      resultDiv.removeChild(roundWinner);
+      humanScore = 0;
+      computerScore = 0;
+      buttons.style.display = "block";
+    });
   }
 }
 
@@ -82,13 +136,4 @@ function getComputerChoice() {
   }
 
   return pcChoice;
-}
-
-function getHumanChoice() {
-  let playerChoice = "";
-  playerChoice = prompt("Choose between Rock, Paper, or Scissors!", "scissors");
-
-  playerChoice = playerChoice.toLowerCase();
-
-  return playerChoice;
 }
